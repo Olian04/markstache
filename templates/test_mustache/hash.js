d5664fa7dfb871d096e2e2fcd648ad2b73451348
Oliver <rift95@live.se>
2018-06-19 14:31:05 +0200

    template fragments now uses global hash scope

--- templates/test_mustache/hash.js
+++ templates/test_mustache/hash.js
@@ -1,20 +1,6 @@
-const common = {
-    port: 8080
-};
 module.exports = {
     title: 'Test',
     description: '',
     "userChanges?": false,
-    fragments: {
-        api_reference: {
-            port: common.port
-        },
-        code_example: {},
-        contributions: {},
-        installation: {},
-        license: {},
-        motivation: {},
-        synopsis: {},
-        tests: {}
-    }
+    port: 8080
 };
